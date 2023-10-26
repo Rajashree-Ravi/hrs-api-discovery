8f3e187cd1a572dd0fb356b72d2ff9d04e6003be
Rajashree Ravi <rajashreeravi09@gmail.com>
2023-10-26 19:24:25 +0530

    API discovery working

---
+++ src/test/java/com/hrs/apidiscovery/ApiDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrs.apidiscovery;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiDiscoveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
